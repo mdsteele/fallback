ced01202bfd51dc2976883eb809b5b2fe276012b
Matthew D. Steele <mdsteele@alum.mit.edu>
2012-06-02 08:43:40 -0400

    Join MonsterAI and MonsterSpells into one package

--- src/Fallback/Scenario/MonsterAI/Spells.hs
+++ src/Fallback/Scenario/MonsterAI/Spells.hs
@@ -17,7 +17,7 @@
 | with Fallback.  If not, see <http://www.gnu.org/licenses/>.                 |
 ============================================================================ -}
 
-module Fallback.Scenario.MonsterSpells
+module Fallback.Scenario.MonsterAI.Spells
   (prepMonsterSpell, getMonsterOpponentPositions, getMonsterVisibility)
 where
 
